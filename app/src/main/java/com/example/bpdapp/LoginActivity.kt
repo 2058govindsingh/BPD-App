7e051ccc75fb4a1f7841d2e4d6b6ec427eb7b9bf
Mayur Ray <raymayur9@gmail.com>
2022-12-13 20:24:10 +0530

    Added login functionality

--- app/src/main/java/com/example/bpdapp/LoginActivity.kt
+++ app/src/main/java/com/example/bpdapp/LoginActivity.kt
@@ -1,11 +1,88 @@
 package com.example.bpdapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Toast
+import com.example.bpdapp.databinding.ActivityLoginBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+
+val TAG = "Login Activity Tag"
 
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLoginBinding
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        auth = Firebase.auth
+
+
+        binding.loginButton.setOnClickListener {
+
+            binding.appBarLayout.visibility = View.GONE
+            binding.loginTphLogo.visibility = View.GONE
+            binding.textFieldEmail.visibility = View.GONE
+            binding.textFieldPassword.visibility = View.GONE
+            binding.loginForgetPassword.visibility = View.GONE
+            binding.loginButton.visibility = View.GONE
+            binding.progressBar.visibility = View.VISIBLE
+
+            val email = binding.inputFieldEmail.text.toString().trim()
+            val password = binding.inputFieldPassword.text.toString()
+
+            auth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        // Sign in success, update UI with the signed-in user's information
+                        Log.d(TAG, "signInWithEmail:success")
+                        val user = auth.currentUser
+                        updateUI(user)
+                    } else {
+                        // If sign in fails, display a message to the user.
+                        Log.w(TAG, "signInWithEmail:failure", task.exception)
+                        Toast.makeText(baseContext, "Authentication failed.",
+                            Toast.LENGTH_SHORT).show()
+                        updateUI(null)
+                    }
+                }
+        }
+    }
+
+    public override fun onStart() {
+        super.onStart()
+        // Check if user is signed in (non-null) and update UI accordingly.
+        val currentUser = auth.currentUser
+        if(currentUser != null){
+            updateUI(currentUser)
+        }
+    }
+
+    private fun updateUI(firebaseUser: FirebaseUser?) {
+        if (firebaseUser != null)
+        {
+            val mainActivityIntent = Intent(this, MainActivity::class.java)
+            startActivity(mainActivityIntent)
+            finish()
+        }
+        else
+        {
+            binding.appBarLayout.visibility = View.VISIBLE
+            binding.loginTphLogo.visibility = View.VISIBLE
+            binding.textFieldEmail.visibility = View.VISIBLE
+            binding.textFieldPassword.visibility = View.VISIBLE
+            binding.loginForgetPassword.visibility = View.VISIBLE
+            binding.loginButton.visibility = View.VISIBLE
+            binding.progressBar.visibility = View.GONE
+        }
     }
 }
--- app/src/main/java/com/example/bpdapp/MainActivity.kt
+++ app/src/main/java/com/example/bpdapp/MainActivity.kt
@@ -1,20 +1,53 @@
 package com.example.bpdapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MenuItem
 import androidx.appcompat.app.ActionBarDrawerToggle
+import com.example.bpdapp.databinding.ActivityMainBinding
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import kotlinx.coroutines.NonCancellable.cancel
 
 class MainActivity : AppCompatActivity() {
-    lateinit var actionBarDrawerToogle :ActionBarDrawerToggle
+
+    private lateinit var binding: ActivityMainBinding
+
+//    lateinit var actionBarDrawerToggle :ActionBarDrawerToggle
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        setUpViews()
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.hamburger.setOnMenuItemClickListener {
+            if (it.itemId == R.id.logOutButton) {
+                MaterialAlertDialogBuilder(this)
+                    .setTitle(resources.getString(R.string.log_out))
+                    .setMessage(resources.getString(R.string.log_out_confirmation))
+                    .setNeutralButton(resources.getString(R.string.cancel)) { dialog, which ->
+                        // Respond to neutral button press
+                        dialog.dismiss()
+                    }
+                    .setPositiveButton(resources.getString(R.string.log_out)) { dialog, which ->
+                        // Respond to positive button press
+                        Firebase.auth.signOut()
+                        val loginActivityIntent = Intent(this, LoginActivity::class.java)
+                        startActivity(loginActivityIntent)
+                        finish()
+                    }
+                    .show()
+                true
+            } else
+                false
+        }
     }
-    fun setUpViews(){
+
+//    fun setUpViews(){
 //        setUpDrawerLayout()
-    }
+//    }
 //    fun setUpDrawerLayout(){
 //        setSupportActionBar(findViewById(R.id.hamburger))
 //        actionBarDrawerToogle=

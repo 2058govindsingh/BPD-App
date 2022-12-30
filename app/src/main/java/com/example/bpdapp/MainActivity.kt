189e0d46699d8f4acf3359dc164afdeb13bd7671
govind <govindyadav2058@gmail.com>
2022-12-30 19:03:41 +0530

    Initial commit to test1

---
+++ app/src/main/java/com/example/bpdapp/MainActivity.kt
@@ -0,0 +1,76 @@
+package com.example.bpdapp
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.app.ActionBarDrawerToggle
+import com.example.bpdapp.databinding.ActivityMainBinding
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import kotlinx.coroutines.NonCancellable.cancel
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+//    lateinit var actionBarDrawerToggle :ActionBarDrawerToggle
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.hamburger.setOnMenuItemClickListener {
+            if (it.itemId == R.id.logOutButton) {
+                MaterialAlertDialogBuilder(this)
+                    .setTitle(resources.getString(R.string.log_out))
+                    .setMessage(resources.getString(R.string.log_out_confirmation))
+                    .setNeutralButton(resources.getString(R.string.cancel)) { dialog, which ->
+                        // Respond to neutral button press
+                        dialog.dismiss()
+                    }
+                    .setPositiveButton(resources.getString(R.string.log_out)) { dialog, which ->
+                        // Respond to positive button press
+                        Firebase.auth.signOut()
+                        val loginActivityIntent = Intent(this, LoginActivity::class.java)
+                        startActivity(loginActivityIntent)
+                        finish()
+                    }
+                    .show()
+                true
+            } else
+                false
+        }
+        binding.searchButton.setOnClickListener{
+            val searchActivityIntent=Intent(this,SearchActivity::class.java)
+            startActivity(searchActivityIntent)
+        }
+        binding.uploadButton.setOnClickListener{
+            val uploadActivityIntent=Intent(this,UploadActivity::class.java)
+            startActivity(uploadActivityIntent)
+        }
+        binding.oneTapActionButton.setOnClickListener{
+            val oneTapActivityIntent=Intent(this,OneTapActivity::class.java)
+            startActivity(oneTapActivityIntent)
+        }
+    }
+
+//    fun setUpViews(){
+//        setUpDrawerLayout()
+//    }
+//    fun setUpDrawerLayout(){
+//        setSupportActionBar(findViewById(R.id.hamburger))
+//        actionBarDrawerToogle=
+//            ActionBarDrawerToggle(this,findViewById(R.id.mainDrawer),R.string.app_name,R.string.app_name)
+//        actionBarDrawerToogle.syncState()
+//    }
+
+//    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+//        if(actionBarDrawerToogle.onOptionsItemSelected(item)){
+//            return true
+//        }
+//        return super.onOptionsItemSelected(item)
+//    }
+}
---
+++ app/src/main/java/com/example/bpdapp/OneTapActivity.kt
@@ -0,0 +1,79 @@
+package com.example.bpdapp
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import androidx.annotation.RequiresApi
+import androidx.appcompat.content.res.AppCompatResources
+import androidx.core.content.res.ResourcesCompat
+import com.example.bpdapp.databinding.ActivityLoginBinding
+import com.example.bpdapp.databinding.ActivityOneTapBinding
+import kotlin.math.roundToInt
+
+class OneTapActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityOneTapBinding
+    private var timerStarted=false
+    private lateinit var serviceIntent:Intent
+    private var time=0.0
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding=ActivityOneTapBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.startStopButton.setOnClickListener{ startStopTimer() }
+        binding.saveButton.setOnClickListener{ saveTimer()}
+        serviceIntent=Intent(this,TimerService::class.java)
+        registerReceiver(updateTime, IntentFilter(TimerService.TIMER_UPDATED))
+    }
+
+    private fun saveTimer() {
+        stopTimer()
+        time=0.0
+        binding.timer.text=getTimeStingFromDouble(time)
+        Toast.makeText(this,"Activity stored in Database Successfully!!",Toast.LENGTH_LONG).show()
+    }
+
+    private fun startStopTimer() {
+        if(timerStarted)
+            stopTimer()
+        else
+            startTimer()
+    }
+    private fun startTimer()
+    {
+        serviceIntent.putExtra(TimerService.TIME_EXTRA,time)
+        startService(serviceIntent)
+        binding.startStopButton.text="Stop"
+        binding.startStopButton.icon=AppCompatResources.getDrawable(this,R.drawable.ic_stop_button)
+        timerStarted=true
+    }
+    private fun stopTimer()
+    {
+        stopService(serviceIntent)
+        binding.startStopButton.text="Start"
+        binding.startStopButton.icon=AppCompatResources.getDrawable(this,R.drawable.ic_start_button)
+        timerStarted=false
+    }
+    private val updateTime: BroadcastReceiver= object :BroadcastReceiver()
+    {
+        override fun onReceive(context: Context, intent: Intent) {
+            time =intent.getDoubleExtra(TimerService.TIME_EXTRA,0.0)
+            binding.timer.text=getTimeStingFromDouble(time)
+        }
+    }
+    private fun getTimeStingFromDouble(time:Double):String
+    {
+        val resultInt=time.roundToInt()
+        val hours=resultInt%86400/3600
+        val minutes=resultInt%86400%3600/60
+        val seconds=resultInt%86400%3600%60
+        return makeTimeString(hours,minutes,seconds)
+    }
+
+    private fun makeTimeString(hour: Int, min: Int, sec: Int): String = String.format("%02d:%02d:%02d",hour,min,sec)
+
+}
